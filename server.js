0c9446174fbfeb3c7f5aa321f9e172619f142966
macikokoro <betwinsouls@gmail.com>
2014-09-07 11:50:56 -0700

    added semicolons to server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-var express = require('express')
+var express = require('express');
 var app = express();
 
 app.get('/hello.txt', function(req, res){
-  res.send('Hello World')
+  res.send('Hello World');
 });
 
 var server = app.listen(3000, function() {
