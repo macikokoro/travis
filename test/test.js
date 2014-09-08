477cd1d297a073becf1e0b3e209a3104840892e3
macikokoro <betwinsouls@gmail.com>
2014-09-07 17:48:00 -0700

    update to test.js added semicolongs to it blocks

--- test/test.js
+++ test/test.js
@@ -7,15 +7,15 @@ describe('Counter Object', function() {
   var counterObject = new counterTest();
 
   it('Should start counting from zero', function() {
-    expect(counterObject.getValue()).to.eql(0);
-  })
+    expect(counterObject.getValue()).to.equal(0);
+  });
 
   it('Should increment count by one', function() {
-    expect(counterObject.increment()).to.eql(1);
-  })
+    expect(counterObject.increment()).to.equal(1);
+  });
 
   it('Should return the current value', function() {
-    // counterObject.increment();
-    expect(counterObject.getValue()).to.eql(1);
-  })
+    counterObject.increment();
+    expect(counterObject.getValue()).to.equal(2);
+  });
 });
