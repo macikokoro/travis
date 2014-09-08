090dcac048c828cb3756680b2192d1c2947e305d
macikokoro <betwinsouls@gmail.com>
2014-09-07 17:38:30 -0700

    adding semicolons to it blocks

--- test/test.js
+++ test/test.js
@@ -8,14 +8,14 @@ describe('Counter Object', function() {
 
   it('Should start counting from zero', function() {
     expect(counterObject.getValue()).to.eql(0);
-  })
+  });
 
   it('Should increment count by one', function() {
     expect(counterObject.increment()).to.eql(1);
-  })
+  });
 
   it('Should return the current value', function() {
     // counterObject.increment();
     expect(counterObject.getValue()).to.eql(1);
-  })
+  });
 });
